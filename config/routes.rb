1a7d903a6ddcdab1f65f67705185eb5bc7f2255f
nahome <nahom.teshome@yahoo.com>
2018-05-17 18:37:52 -0700

    authorization2

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   namespace :api, defaults: { format: :json } do
-     resources :users
+     resources :users, only: :index
    end
 
   # The priority is based upon order of creation: first created -> highest priority.
